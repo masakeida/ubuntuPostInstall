99643dfead0c6743c97941cacafee3c3f9cc8a9d
Masayuki Keida <masayuki.keida+github@gmail.com>
2022-01-16 13:12:07 +0000

    first commit

---
+++ disableServers.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+usage() {
+    cat <<EOF
+Usage:
+    $(basename ${0}) server_list_file
+EOF
+}
+
+version() {
+    echo "$(basename ${0}) version 0.0.1"
+}
+
+if [ $# -ne 1 ]; then
+    version
+    usage
+    exit 1
+fi
+
+filename=$1
+
+if [ ! -e ${filename} ]; then
+    echo "${filename} does not exist. Stopped."
+    exit 1
+fi
+
+#export ASSUME_ALWAYS_YES=yes
+
+cat ${filename} | grep -v "^#" | grep -v "^$" | while read line
+do
+    systemctl stop ${line}
+    systemctl disable ${line}
+done
---
+++ ubuntuPostInstall.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+usage() {
+    cat <<EOF
+Usage:
+    $(basename ${0}) [<options>]
+Options:
+    --version, -v     print $(basename ${0}) version
+    --vmware, -w      configure for vmware
+EOF
+}
+
+version() {
+    cat <<EOF
+$(basename ${0}): 20220115
+EOF
+}
+
+# default vmware flag is no
+VMWARE=NO
+
+while [ $# -gt 0 ];
+do
+    case ${1} in
+
+	--version|-v)
+	    version
+	    exit 0
+	    ;;
+
+	--vmware|-w)
+	    VMWARE=YES
+	    ;;
+
+	*)
+	    echo "Invalid argment '${1}'"
+	    usage
+	    exit 1
+	    ;;
+
+    esac
+    shift
+done
+
+# get login name by uid 1000
+# it is not used below. it is for the future.
+NUSER=`id -u -n 1000`
+
+# modify pkg_list.txt for vmware
+if [ ${VMWARE} = "YES" ]; then
+    cat >> pkg_list.txt <<EOF
+# vmware tools
+open-vm-tools-desktop
+EOF
+fi
+
+# update
+apt-get update
+apt-get upgrade
+apt-get dist-upgrade
+
+# packages
+./aptgetInstaller.sh pkg_list.txt
+
+# disable server suite
+./disableServers.sh server_list.txt
