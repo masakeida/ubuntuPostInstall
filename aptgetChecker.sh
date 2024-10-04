c159eeaae887e190a3635ed0b669eb49e412b034
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-10-04 20:52:23 +0900

    for 24.04

---
+++ aptgetChecker.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+usage() {
+    cat <<EOF
+Usage:
+    $(basename ${0}) pkg_list_file
+EOF
+}
+
+version() {
+    echo "$(basename ${0}) version 0.0.1"
+}
+
+if [ $# -ne 1 ]; then
+    version
+    usage
+    exit 1
+fi
+
+filename=$1
+
+if [ ! -e ${filename} ]; then
+    echo "${filename} does not exist. Stopped."
+    exit 1
+fi
+
+#export ASSUME_ALWAYS_YES=yes
+
+cat ${filename} | grep -v "^#" | grep -v "^$" | while read line
+do
+    apt-get -y -s install ${line}
+done
